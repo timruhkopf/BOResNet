85754d47789ed6ec1a596e7eb4ed7de91a612576
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-06-07 17:27:54 +0200

    created ResidualBlock.py & ResNet9.py list-layers concept

--- src/ResNet9.py
+++ src/ResNet9.py
@@ -1,15 +1,22 @@
+"""
+Author: Tim Ruhkopf
+License:
+"""
+
 import torch
+import torch.nn as nn
 
 
 class ResNet9:
     def __init__(self):
         # build layer design
-
-
         pass
 
     def forward(self, X):
-        return
+        # path through ResNet-Conv blocks
+
+        # class prediction
+        return nn.Softmax()
 
 
 if __name__ == '__main__':
--- src/ResidualBlock.py
+++ src/ResidualBlock.py
@@ -1,10 +1,44 @@
 import torch
+import torch.nn as nn
+import itertools
 
 
-class ResidualBlock:
-    def __init__(self):
-        pass
+class ResidualBlock(nn.Module):
+    def __init__(self, hunits, skip=2):
+        """
+        Same sized Convolution
+        :param skip: int. >1
+        :param hunits:
+        """
+        super().__init__()
+
+        if skip < 2:
+            msg = 'According to Deep Residual Learning for Image Recognition by' \
+                  'He et. al 2015, skips < 2 are not reasonable.'
+            raise ValueError(msg)
+
+        self.skip = skip
+
+        # comprehension to efficiently expand the (conv2d + BN + relu) to the
+        # desired number of these units - dependent on the hunits parameter.
+        self.layers = list(itertools.chain.from_iterable(
+            (nn.Conv2d(*config), nn.BatchNorm2d(), nn.ReLU()) for config in
+            hunits))
+
+    def forward(self, X):
+        for i, l in enumerate(self.layers):
+
+            if i // 3 * self.skip == 0:
+                # layers come in ordering (conv2d, BN, ReLU) and are repeated
+                # skip number of times before a skip connection is established
+                out = l(X) + X
+                X = out
+
+            else:
+                out = l(X)
+
+        return
 
 
 if __name__ == '__main__':
-    pass
+    ResidualBlock(skip=3, )
