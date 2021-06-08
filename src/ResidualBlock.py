935e7a0e7199e3a2e4513771fd72650c9c04204b
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-06-08 12:06:11 +0200

    ResidualBlock.py: (working) scalable version incl. tests

--- src/ResidualBlock.py
+++ src/ResidualBlock.py
@@ -1,44 +1,88 @@
-import torch
 import torch.nn as nn
 import itertools
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, hunits, skip=2):
+    def __init__(self, cunits, kernel=3):
         """
         Same sized Convolution
-        :param skip: int. >1
-        :param hunits:
+        implements a scalable version of  the dashed and solid line residual
+        skip connection blocks from
+
+        @inproceedings{he2016deep,
+          title={Deep residual learning for image recognition},
+          author={He, Kaiming and Zhang, Xiangyu and Ren,
+                  Shaoqing and Sun, Jian},
+          booktitle={Proceedings of the IEEE conference on computer vision
+                     and pattern recognition},
+          pages={770--778},
+          year={2016}
+        }
+
+        In case of cunits = (64, 64, 64), this amounts to a skip over two (
+        no_skips = len(cunits)-1) convolutions:
+
+        ----------------------------------
+        |                                |
+        x --> conv, bn, relu, conv, bn + x , relu ---> y
+
+        In case of cunits = (64, 64, 64, 64), a skip over three convolutions
+        -------------------------------------------------
+        |                                                |
+        x --> conv, bn, relu, conv, bn, relu, conv, bn + x , relu ---> y
+
+
+        :param cunits: tuple of consecutive convolutional channels, describing
+        the entire residblock's convolutions.
+        Starting with no. of channels in the residblock's input tensor,
+        and continuing with the consecutive channels introduced by convolution.
+
+        e.g. (1, 64, 128) implies input tensor has 1 channel,
+        so the first conv layer has 1 "in" channel and 64 "out" channels.
+        the second conv layer then automatically has 64 "in" channels.
+        The specified 128 implies this conv layer produces 128 "out" channels.
+
+        In this scenario of unequal no. of channels between beginning and
+        last conv., the skip connection cannot be identity, as the same
+        sized images (same convolution) have a different number of channels.
+        To adjust the size, a 1x1 convolution of appropriate no. of channels
+        is used as skip connection before adding the scaled X to the
+        residblock's output.
+        :param kernel: the squared filter size used in all convolutions in
+        the residblock.
         """
+
+        # setup nn.Module's bookkeeping
         super().__init__()
 
-        if skip < 2:
-            msg = 'According to Deep Residual Learning for Image Recognition by' \
-                  'He et. al 2015, skips < 2 are not reasonable.'
-            raise ValueError(msg)
+        # save arguments for later usage
+        self.cunits = cunits
+        self.kernel = kernel
 
-        self.skip = skip
-
-        # comprehension to efficiently expand the (conv2d + BN + relu) to the
-        # desired number of these units - dependent on the hunits parameter.
+        # scalable version of repetitive parts (conv-bn-relu) to skip over
+        # notice, that relu are applied in forward path!
         self.layers = list(itertools.chain.from_iterable(
-            (nn.Conv2d(*config), nn.BatchNorm2d(), nn.ReLU()) for config in
-            hunits))
+            (nn.Conv2d(no_in, no_out, kernel_size=kernel, padding=1),
+             nn.BatchNorm2d(no_out),
+             nn.ReLU())
+            for no_in, no_out in zip(cunits[:-2], cunits[1:-1])))
 
-    def forward(self, X):
-        for i, l in enumerate(self.layers):
+        # final (conv-bn-skip-relu)
+        # add identity or 1x1 conv depending on the shape-change in residblock
+        self.layers.extend([
+            nn.Conv2d(cunits[-2], cunits[-1], kernel, padding=1),
+            nn.BatchNorm2d(cunits[-1]),
+            # skip connection; either identity or 1x1 conv to adjust shape
+            nn.Identity() if cunits[0] == cunits[-1] else \
+                nn.Conv2d(cunits[0], cunits[-1], 1),
+            nn.ReLU()])
 
-            if i // 3 * self.skip == 0:
-                # layers come in ordering (conv2d, BN, ReLU) and are repeated
-                # skip number of times before a skip connection is established
-                out = l(X) + X
-                X = out
+    def forward(self, x):
+        xskip = x
 
-            else:
-                out = l(X)
+        # skipped block until skip connection
+        for layer in self.layers[:-2]:
+            x = layer(x)
 
-        return
-
-
-if __name__ == '__main__':
-    ResidualBlock(skip=3, )
+        # relu(X+ ident(X_skip) or  relu(X+ conv1x1(X_skip).
+        return self.layers[-1](x + self.layers[-2](xskip))
--- test/test_ResidualBlock.py
+++ test/test_ResidualBlock.py
@@ -1,9 +1,68 @@
 import unittest
+import torch
+import torch.nn as nn
+from src.utils import load_npz_kmnist
+import os
+
+from src.ResidualBlock import ResidualBlock
 
 
 class Test_ResidualBlock(unittest.TestCase):
-    def test_(self):
-        pass
+
+    def no_convs(self, model):
+        return len([l for l in model.layers if isinstance(l, nn.Conv2d)])
+
+    def setUp(self) -> None:
+        os.chdir("..")
+        os.getcwd()
+        root_data = 'Data/Raw/'
+        self.x_train, self.y_train = load_npz_kmnist(
+            folder=root_data,
+            files=['kmnist-train-imgs.npz', 'kmnist-train-labels.npz'])
+
+        self.y_train = torch.unsqueeze(self.y_train, dim=1)
+        self.x_train = torch.reshape(self.x_train, (-1, 1, 28, 28))
+        print('train_shape', self.x_train.shape)
+
+    def test_forward_dimensions(self):
+        residblock = ResidualBlock(
+            cunits=(1, 64, 64, 128),
+            kernel=3)
+
+        self.assertEqual(residblock.forward(self.x_train[:1]).shape, \
+                         torch.Size([1, 128, 28, 28]))
+
+    def test_no_of_convolutions(self):
+        """check that the scalable residblock actually has the appropriate
+        number of convolutions."""
+
+        # unequal sized skip implies 1x1 convoution on skipped image
+        # case 1: 3 convolutions + 1x1 convolution: 4 total
+        residblock = ResidualBlock(
+            cunits=(1, 64, 64, 64),
+            kernel=3)
+
+        self.assertEqual(self.no_convs(residblock), 4,
+                         'number of convolutions are incorrect')
+
+        residblock = ResidualBlock(
+            cunits=(64, 64, 64),
+            kernel=3)
+
+        self.assertEqual(self.no_convs(residblock), 2,
+                         'number of convolutions are incorrect')
+
+    def test_cunits_placed_to_convs(self):
+        residblock = ResidualBlock(
+            cunits=(1, 2, 3),
+            kernel=3)
+
+        channels = [(l.in_channels, l.out_channels)
+                    for l in residblock.layers \
+                    if isinstance(l, nn.Conv2d)]
+
+        # notice that the 1x1 convolution on the skip applies with (1,3)
+        self.assertEqual(channels, [(1, 2), (2, 3), (1, 3)])
 
 
 if __name__ == '__main__':
