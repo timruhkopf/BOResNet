ca0d160418f8f79aa80ce26ef92a90b61e78cfb8
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-06-09 12:25:51 +0200

    BO.py Added new skeleton incl. incumbent logic

--- src/BO.py
+++ src/BO.py
@@ -1,26 +1,159 @@
 import torch
-
 import torch.nn as nn
+import torch.distributions as td
 from torch.optim import SGD
 
+import pyro
+import pyro.contrib.gp as gp
+import pyro.distributions as dist
 
-class BayesianOptimizer(torch.optim):
-    def __init__(self, budget):
+from math import pi
+import matplotlib.pyplot as plt
+
+pyro.set_rng_seed(0)
+
+
+# TODO Seeding
+
+class BayesianOptimizer:
+    def __init__(self, search_space, budget, closure):
+        """
+        BO working on a 1d search space
+        :param search_space: tuple of floats, giving the interval bounds of
+        the one dimensional continuous search space.
+        :param budget: int. number of function evaluations.
+        :param closure: parametrized & callable function that is to be
+        optimized
+        """
         # TODO check how to instantiate the model anew for each evaluation
         #  of SGD with a specific learning rate!
         self.budget = budget
-        pass
+        self.closure = closure
+        self.search_space = search_space
 
-    def gaussian_process(self):
+        # sample the first data point at random.
+        self.inquired = torch.zeros(budget)
+        self.cost = torch.zeros(budget)
+
+        # TODO consider prior knowledge: different function
+        self.inquired[0] = td.Uniform(*search_space).sample([1])
+        self.cost[0] = closure(self.inquired[0])
+        self.incumbent = self.inquired[0]
+        self.inc_idx = 0
+
+    def gaussian_process(self, X, y):
         """library for gp: https://pyro.ai/examples/gp.html"""
-        pass
 
-    def expected_improvement(self):
-        """function definition based on the lecture slides"""
+        kernel = gp.kernels.RBF(input_dim=1, variance=torch.tensor(5.),
+                                lengthscale=torch.tensor(10.))
+
+        # TODO consider Exponential kernel instead
+        # kernel = gp.kernels.Exponential()
+        # TODO consider noise < 1 to smaller uncertainty around observed
+        #  points.
+        gpr = gp.models.GPRegression(X, y, kernel, noise=torch.tensor(1.))
+
+        optimizer = torch.optim.Adam(gpr.parameters(), lr=0.005)
+        loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
+        losses = []
+        num_steps = 2000
+        for i in range(num_steps):
+            optimizer.zero_grad()
+            loss = loss_fn(gpr.model, gpr.guide)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+        plt.plot(losses)
+
+        gpr.kernel.variance.item()
+        gpr.kernel.lengthscale.item()
+        gpr.noise.item()
+
+        # now do MAP estimation:
+        # Define the same model as before.
+        pyro.clear_param_store()
+        kernel = gp.kernels.RBF(input_dim=1, variance=torch.tensor(5.),
+                                lengthscale=torch.tensor(10.))
+        gpr = gp.models.GPRegression(X, y, kernel, noise=torch.tensor(1.))
+
+        # note that our priors have support on the positive reals
+        gpr.kernel.lengthscale = pyro.nn.PyroSample(dist.LogNormal(0.0, 1.0))
+        gpr.kernel.variance = pyro.nn.PyroSample(dist.LogNormal(0.0, 1.0))
+
+        optimizer = torch.optim.Adam(gpr.parameters(), lr=0.005)
+        loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
+        losses = []
+        num_steps = 2500
+        for i in range(num_steps):
+            optimizer.zero_grad()
+            loss = loss_fn(gpr.model, gpr.guide)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+    def gp_plot(self, X, y, model=None, n_test=500):
+        """
+
+        :param X:
+        :param y:
+        :param model:
+        :param n_test:
+        :return:
+        """
+
+        plt.figure(figsize=(12, 6))
+        # plot the datapoints
+        plt.plot(X.numpy(), y.numpy(), 'kx')
+
+        # generate points at which gp is evaluated at for plotting
+        Xtest = torch.linspace(*self.search_space, n_test)
+
+        # compute predictive mean and variance for each of these points
+        with torch.no_grad():
+            mean, cov = model(Xtest, full_cov=True, noiseless=False)
+
+        # standard deviation at each input point x (testpoints)
+        sd = cov.diag().sqrt()
+
+        # plot the mean prediction
+        plt.plot(Xtest.numpy(), mean.numpy(), 'r', lw=2)
+
+        # plot the two-sigma uncertainty about the mean
+        plt.fill_between(Xtest.numpy(),
+                         # "confidence-bands"
+                         (mean - 2.0 * sd).numpy(),
+                         (mean + 2.0 * sd).numpy(),
+                         color='C0', alpha=0.3)
+
+        plt.xlim(*self.search_space)
+
+        # TODO add acquisition function & display incumbent
+
+    def expected_improvement(self, lamb, eps):
+        """
+        function definition based on the lecture slides
+        https://learn.ki-campus.org/courses/automl-luh2021/items/7rd8zSXREMWYBfbVTXLcci
+        """
+
+        # PSEUDO CODE:
+        # mu = None
+        # sigma = None
+        # Z = (self.cost[self.inc_idx] - mu(lamb) + eps) / sigma(lamb)
+        #
+        # # if sigma(lamb) >0
+        # u_t = sigma(lamb) * (Z * td.Normal.cdf(Z) +
+        #                      torch.exp(td.Normal.log_prob(lamb)) +
+        #                      # Normalizing constant std. Normal:
+        #                      torch.tensor([2. * pi]) ** -1 / 2)
+        #
+        # else sigma(lamb) =0
+        # u_t = torch.tensor([0.])
         pass
 
     def bo_loop(self):
-        """pseudo code:
+        """
+        pseudo code:
 
         Require: Search space Λ , cost function c, acquisition function u, pre-
             dictive model ĉ, maximal number of function evaluations T
@@ -36,9 +169,30 @@ class BayesianOptimizer(torch.optim):
             Update data: D^(t) ← D^(t−1) ∪ {<λ^(t) , c(λ^(t))>}
 
         return arg_min_λ c(λ^(t)) from {λ_t}_t=1 ^T
-        the return value is called the INCUMBANT
+        the return value is called the INCUMBENT
         """
-        pass
+
+        for t in range(1, self.budget):
+            # Fit predictive model
+            self.gaussian_process(X=self.inquired[:t], y=self.cost[:t])
+
+            # Select next query point:
+            # optimize over self.expected_improvement() to find max value lamb.
+            lamb = None
+            self.inquired[t] = lamb
+
+            # Query cost function
+            self.cost[t] = self.closure(lamb)
+
+            # replace the incumbent if necessary
+            # TODO check if we deal with loss, that min on neg is correct.
+            #  this is a call for a convention on max / min the function!
+            self.incumbent, self.inc_idx, _ = min([
+                (self.incumbent, self.inc_idx, self.cost[self.inc_idx]),
+                (lamb, t, self.cost[t])],
+                key=lambda x: x[2])
+
+        return self.incumbent
 
     def evaluate_model_with_SGD(self, model, dataloader, epochs, lr):
         """
--- test/test_BO.py
+++ test/test_BO.py
@@ -1,14 +1,17 @@
 import unittest
 import torch
+from src.BO import BayesianOptimizer
 
 
 class Test_BO(unittest.TestCase):
 
     @staticmethod
-    def f(lamb):
-        """black box function that is easy to query for testing
+    def c(lamb):
+        """
+        black box function that is easy to query for testing
         :param lamb: float32,
-        :return torch.Tensor of dtype torch.float32 """
+        :return torch.Tensor of dtype torch.float32
+        """
         return torch.tensor([lamb ** 4 - 8 * lamb ** 3 - 12 * lamb + 24],
                             dtype=torch.float32)
 
@@ -19,9 +22,9 @@ class Test_BO(unittest.TestCase):
         pass
 
     def test_(self):
-        Test_BO.f(2.)
-
-        self.assertEqual(True, False, msg='')
+        # Test_BO.c(2.)
+        BayesianOptimizer(search_space=(0.001, 0.1), budget=10)
+        # self.assertEqual(True, False, msg='')
 
 
 if __name__ == '__main__':
