21824c1e4fe595aac637aaa8fc7bf12491ef25a3
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-06-14 20:32:51 +0200

    Setup fullrun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import TensorDataset, DataLoader
-
+import pyro
 import matplotlib
 import os
 
@@ -9,14 +9,18 @@ from src.utils import load_npz_kmnist, plot_kmnist
 from src.RUNS import RUNS
 from src.BO import BayesianOptimizer
 
+# setup your computation device / plotting method
 matplotlib.use('TkAgg')
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-batch_size = 10
+# seeding for reproducibility
+pyro.set_rng_seed(0)
+torch.manual_seed(0)
 
 # (0) loading data & preporcessing according to
 # https://github.com/rois-codh/kmnist/blob/master/benchmarks/kuzushiji_mnist_cnn.py
 # Load the data
+batch_size = 4
 root_data = 'Data/Raw/'
 x_train, x_test, y_train, y_test = load_npz_kmnist(
     folder=root_data,
@@ -24,12 +28,13 @@ x_train, x_test, y_train, y_test = load_npz_kmnist(
            'kmnist-train-labels.npz', 'kmnist-test-labels.npz'])
 
 # testing if training starts at all
+
 # FIXME: change this back to the full dataset!
-n = 100  # len(x_train)
-x_train = x_train[:n]
-y_train = y_train[:n]
-x_test = x_test[:int(n/10)]
-y_test = y_test[:int(n/10)]
+# n = 100  # len(x_train)
+# x_train = x_train[:n]
+# y_train = y_train[:n]
+# x_test = x_test[:int(n / 10)]
+# y_test = y_test[:int(n / 10)]
 
 # plot an example image
 # plot_kmnist(x_train, y_train,
@@ -68,50 +73,46 @@ testloader = DataLoader(testset, batch_size=batch_size,
 # x, y = next(iterable)
 
 # setup the model
-# skip = 2
 # resnet9 = ResNet(
 #     img_size=(28, 28),
 #     architecture=((1, 8), (8, 8, 8), (8, 8, 8),
 #                   (8, 8, 8), (8, 16, 16)),
 #     no_classes=10)
 
-resnet3 = ResNet(
-    img_size=(28, 28),
-    architecture=((1, 8), (8, 8, 8), (8, 16, 16)),
-    no_classes=10)
-
-#
-# resnet9 = ResNet(
+# resnet3 = ResNet(
 #     img_size=(28, 28),
-#     architecture=((1, 16), (16, 16, 16), (16, 16, 16), (32, 32, 32),
-#                   (32, 32, 32), (32,64,64),
+#     architecture=((1, 8), (8, 8, 8), (8, 16, 16)),
 #     no_classes=10)
+
+
+resnet9 = ResNet(
+    img_size=(28, 28),
+    architecture=((1, 16), (16, 16, 16), (16, 16, 16), (32, 32, 32),
+                  (32, 32, 32), (32, 64, 64)),
+    no_classes=10)
 
 # resnet3.to(device)
 
 # create, track & run a model with sgd under a specific learning rate
 root = os.getcwd()
-runs = RUNS(resnet3, trainloader, testloader, epochs=2,
-            path=root + '/models/run1/model_')
+runs = RUNS(resnet9, trainloader, testloader, epochs=2,
+            path=root + '/models/fullrun/model_')
 
-
-
-runs.evaluate_model_with_SGD(lr=0.001)
+# runs.evaluate_model_with_SGD(lr=0.001)
 # runs.evaluate_model_with_SGD(lr=0.005)
 # runs.evaluate_model_with_SGD(lr=0.003)
 
 
 # pass closure object to BO
-bo = BayesianOptimizer(search_space=(0.001, 0.01),
-                       budget=5,  # FIXME: change this
+bo = BayesianOptimizer(search_space=(0.001, 0.02),
+                       budget=10,  # FIXME: change this
                        closure=runs.evaluate_model_with_SGD)
-bo.optimize(eps=0., initial_lamb=0.003)
+bo.optimize(eps=0., initial_lamb=0.01)
 
 root = os.getcwd()
-bo.fig.legend() # FIXME: add legend to the plot
-bo.fig.savefig(root +"/Plots/bo_run1.pdf", bbox_inches='tight')
+# bo.fig.legend()  # FIXME: add legend to the plot
+bo.fig.savefig(root + "/Plots/bo_fullrun.pdf", bbox_inches='tight')
 print()
-
 
 # TODO write out the BO images & check for single run on entire dataset,
 #  multiple epochs
