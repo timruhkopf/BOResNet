dae2764c514f88fa8505142d88a8ebd18989649b
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2021-06-17 13:07:31 +0200

    Main.py Fullrun config including pickle

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from torch.utils.data import TensorDataset, DataLoader
 import pyro
 import matplotlib
 import os
+import pickle
+from pathlib import Path
 
 from src.resnet import ResNet
 from src.utils import load_npz_kmnist, plot_kmnist
@@ -13,7 +15,18 @@ from src.bo import BayesianOptimizer
 matplotlib.use('Agg')
 BATCH_SIZE = 4
 ROOT_DATA = 'Data/Raw/'
+RUNIDX = 1
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+bo_config = dict(
+    search_space=(0.001, 0.02),
+    budget=10)
+
+resnet_config = dict(img_size=(28, 28),
+                     architecture=(
+                         (1, 16), (16, 16, 16), (16, 16, 16), (16, 32, 32),
+                         (32, 32, 32), (32, 64, 64)),
+                     no_classes=10)
 
 # seeding for reproducibility
 pyro.set_rng_seed(0)
@@ -35,12 +48,12 @@ x_train, x_test, y_train, y_test = load_npz_kmnist(
 
 # testing if training starts at all
 
-# FIXME: change this back to the full dataset!
-n = 10000  # len(x_train)
-x_train = x_train[:n]
-y_train = y_train[:n]
-x_test = x_test[:int(n / 10)]
-y_test = y_test[:int(n / 10)]
+# for testing purposes
+# n = 100  # len(x_train)
+# x_train = x_train[:n]
+# y_train = y_train[:n]
+# x_test = x_test[:int(n / 10)]
+# y_test = y_test[:int(n / 10)]
 
 # plot an example image
 # plot_kmnist(x_train, y_train,
@@ -63,7 +76,7 @@ y_test = y_test.type(torch.LongTensor)
 x_train = torch.unsqueeze(x_train, dim=1)
 x_test = torch.unsqueeze(x_test, dim=1)
 
-# descriptive info
+# descriptive info of the dataset
 print("y's shape: {}\nx's shape: {}".format(y_train.shape, x_train.shape))
 
 # create Dataset & dataloader for Train & Test.
@@ -77,56 +90,65 @@ testloader = DataLoader(
     testset, batch_size=BATCH_SIZE,
     shuffle=True, num_workers=1)
 
-# iterable = testloader.__iter__()
-# x, y = next(iterable)
-
 # setup the model
 # resnet9 = ResNet(
 #     img_size=(28, 28),
 #     architecture=((1, 8), (8, 8, 8), (8, 8, 8),
 #                   (8, 8, 8), (8, 16, 16)),
 #     no_classes=10)
-
-resnet3 = ResNet(
-    img_size=(28, 28),
-    architecture=((1, 8), (8, 8, 8)),
-    no_classes=10)
-
-# resnet9 = ResNet(
-#     img_size=(28, 28),
-#     architecture=((1, 16), (16, 16, 16), (16, 16, 16), (16, 32, 32),
-#                   (32, 32, 32), (32, 64, 64)),
-#     no_classes=10)
 #
-# resnet9.to(DEVICE)
+# resnet3 = ResNet(
+#     img_size=(28, 28),
+#     architecture=((1, 8), (8, 8, 8)),
+#     no_classes=10)
+
+resnet = ResNet(**resnet_config)
+resnet.to(DEVICE)
 
 # create, track & run a model with sgd under a specific learning rate
 root = os.getcwd()
-runs = BlackBoxPipe(
-    resnet3, trainloader, testloader, epochs=2,
-    path=root + '/models/fullrun/model_')
+modeldir = root + '/models/fullrun{}'.format(RUNIDX)
+Path(modeldir).mkdir(parents=True, exist_ok=True)
+pipe = BlackBoxPipe(
+    resnet, trainloader, testloader, epochs=5,
+    path=modeldir + '/model_')
 
-# runs.evaluate_model_with_SGD(lr=0.001)
-# runs.evaluate_model_with_SGD(lr=0.005)
-# runs.evaluate_model_with_SGD(lr=0.003)
+# check consecutive runs are tracked
+# pipe.evaluate_model_with_SGD(lr=0.001)
+# pipe.evaluate_model_with_SGD(lr=0.005)
 
-
-# pass closure object to BO
+# pass closure object to BO, which is the bridge from the model to bo
 bo = BayesianOptimizer(
-    search_space=(0.001, 0.02),
-    budget=10,  # FIXME: change this
-    closure=runs.evaluate_model_with_SGD)
+    **bo_config,
+    closure=pipe.evaluate_model_with_SGD)
 
 bo.optimize(eps=0., initial_lamb=0.01)
 
+# write out the final image
 root = os.getcwd()
-# bo.fig.legend()  # FIXME: add legend to the plot
-bo.fig.savefig(root + '/Plots/bo_fullrun.pdf', bbox_inches='tight')
-# print()
+bo.fig.savefig(root + '/Plots/bo_fullrun{}.pdf'.format(RUNIDX),
+               bbox_inches='tight')
 
-#
-# # TODO write out the BO images & check for single run on entire dataset,
-# #  multiple epochs
+# write out the configs & interesting rundata
+pickledict = dict(
+    resnet_confit=resnet_config,
+    bo_config=bo_config,
+    losses=pipe.trainlosses,
+    incumbent=bo.incumbent,
+    costs=bo.costs,
+    inquired=bo.inquired,
+    accuracy=bo.acc)
+
+modeldir = root + '/models/pickle/'.format(RUNIDX)
+Path(modeldir).mkdir(parents=True, exist_ok=True)
+filename = modeldir + '/fullrun{}.pkl'.format(RUNIDX)
+with open(filename, 'wb') as handle:
+    pickle.dump(pickledict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# with open('filename.pickle', 'rb') as handle:
+#     b = pickle.load(handle)
+
+
 # # deprec
 # import matplotlib.pyplot as plt
 #
@@ -134,5 +156,3 @@ bo.fig.savefig(root + '/Plots/bo_fullrun.pdf', bbox_inches='tight')
 #
 # bo.inquired
 # runs.lrs
-#
-# # TODO: check that proposed !!!!!
